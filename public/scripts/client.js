2c008baa95892042b96df121d73a1a0402adb9d4
Vivian Trinh <vptrinh@ryerson.ca>
2022-02-02 18:50:44 -0500

    Bug fixes.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,31 +7,6 @@
 
 $(document).ready(function(){
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
-
   //Creates a new tweet element using data from the object. 
 
   const createTweetElement = function (data) {
@@ -68,10 +43,13 @@ $(document).ready(function(){
     }
   }
 
-  renderTweets(data);
-  // const $tweet = createTweetElement(tweetData);
-  // $('#tweets-container').append($tweet);
-  // console.log($tweet);
+  const loadTweets = function () {
+    $.ajax('/tweets', {method: 'GET'})
+    .then(function (tweets) {
+      console.log('Success: ', tweets);
+      renderTweets(tweets)
+    })
+  }
 
   $("#submit").submit(function(event) {
     console.log("Form submitted.")
@@ -80,5 +58,7 @@ $(document).ready(function(){
     $.post('/tweets', param).then(() => {
     })
   });
+
+  loadTweets();
 });
 
