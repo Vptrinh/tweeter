d8ea61c4c86b3b277043f1ae664f2565c8534c6d
Vivian Trinh <vptrinh@ryerson.ca>
2022-02-02 16:21:32 -0500

    Added a create tweet element function that links to the objects. Fixed bugs.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,44 +4,49 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const createTweetElement = function () {
-  const $tweet = $(`<article class = "tweet">
-  <header>
-    <span>
-      <img src="/images/profile-hex.png">
-      <span>Newton</span>
-    </span>
-    <span class="username">@YourName</span>
-  </header>
-  <section>
-    <span class="text"><b>If I have seen further it is by standing on the shoulder of giants.</b></span>
-  </section>
-  </body>
-  <footer>
-    <span class="timestamp">10 days ago</span>
-    <span class="icons">
-      <i class="fa-solid fa-flag"></i>
-      <i class="fa-solid fa-retweet"></i>
-      <i class="fa-solid fa-heart"></i>
-    </span>
-  </footer>
-</article>`);
-}
 
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
+$(document).ready(function(data){
+
+  const tweetData = {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
       "handle": "@SirIsaac"
-    },
-  "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-  "created_at": 1461116232227
-}
+      },
+    "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+    "created_at": 1461116232227
+  };
 
-const $tweet = createTweetElement(tweetData);
+  const createTweetElement = function (data) {
+    const $tweet = `
+    <article class = "tweet">
+      <header>
+      <span>
+        <img src="${data.user.avatars}">
+        <span class="name">${data.user.name}</span>
+      </span>
+      <span class="username">${data.user.handle}</span>
+    </header>
+    <section>
+      <span class="text"><b>${data.content.text}</b></span>
+    </section>
+    </body>
+    <footer>
+      <span class="timestamp">${timeago.format(data.created_at)}</span>
+      <span class="icons">
+        <i class="fa-solid fa-flag"></i>
+        <i class="fa-solid fa-retweet"></i>
+        <i class="fa-solid fa-heart"></i>
+      </span>
+    </footer>
+  </article>`;
+  return $tweet;
+  }
+  
+  const $tweet = createTweetElement(tweetData);
+  $('#tweets-container').append($tweet);
+  console.log($tweet);
+});
 
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
